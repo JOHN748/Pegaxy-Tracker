bf940deb7310c68dd921321562909e26afd306b7
JOHN 748 <48284893+JOHN748@users.noreply.github.com>
2022-05-08 15:31:26 +0530

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Website Title -->
+    <title>Error 404</title>
+    <!-- Meta Tags -->
+    <?php include 'includes/header/meta-tags.php'; ?>
+    <!-- Default CSS -->
+    <?php include 'includes/header/header-styles.php'; ?>    
+</head>
+
+<body>
+
+    <div class="account-pages my-5 pt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-center mb-5">
+                        <h1 class="display-2 fw-medium">4<i class="bx bx-buoy bx-spin text-primary display-3"></i>4</h1>
+                        <h4 class="text-uppercase">Sorry, page not found</h4>
+                        <div class="mt-5 text-center">
+                            <a class="btn btn-primary waves-effect waves-light" href="index">Back to Home Page</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-xl-6">
+                    <div>
+                        <img src="assets/images/assets/error-img.png" alt="" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!-- Default JS -->
+<?php include 'includes/footer/footer-scripts.php'; ?>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!-- HTML Start -->
+<!DOCTYPE html>
+<html lang="en">
+<!-- Head Section -->
+<head>
+    <!-- Website Title -->
+    <title>Pegaxy Portfolio</title>
+    <!-- Meta Tags -->
+    <?php include 'includes/header/meta-tags.php'; ?>
+    <!-- Default CSS -->
+    <?php include 'includes/header/header-styles.php'; ?>
+    <!-- Script JS -->
+    <?php include 'includes/header/header-scripts.php'; ?>
+</head>
+
+<!-- Body Section -->
+<body data-sidebar="dark">
+
+<!-- Begin page -->
+<div id="layout-wrapper">
+
+    <!-- MenuBar -->
+    <?php include 'includes/menu/menu.php'; ?>
+
+    <!-- Main Content -->
+    <div class="main-content">
+        
+        <!-- Page Content -->
+        <div class="page-content">
+            
+            
+
+            </div>
+            <!-- End Page Container -->
+        </div>
+        <!-- End Page Content -->
+
+        <?php include 'includes/footer/footer.php'; ?>
+
+    </div>
+    <!-- End Main Content -->
+
+</div>
+<!-- End layout Wrapper -->
+
+<!-- Right Sidebar -->
+<?php include 'includes/menu/right-sidebar.php'; ?>
+
+<!-- Default JS -->
+<?php include 'includes/footer/footer-scripts.php'; ?>
+
+</body>
+</html>
+
